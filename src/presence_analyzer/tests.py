18338d93a3eb9463cc06a0fa7c7d01fb96a12f2f
KarolJagodzinski <karol.jagodzinski@stxnext.pl>
2015-11-12 16:28:59 +0100

    Added unit tests

--- src/presence_analyzer/tests.py
+++ src/presence_analyzer/tests.py
@@ -2,6 +2,8 @@
 """
 Presence analyzer unit tests.
 """
+from __future__ import unicode_literals
+
 import os.path
 import json
 import datetime
@@ -11,8 +13,7 @@ from presence_analyzer import main, views, utils
 
 
 TEST_DATA_CSV = os.path.join(
-    os.path.dirname(__file__), '..', '..', 'runtime', 'data', 'test_data.csv'
-)
+    os.path.dirname(__file__), '..', '..', 'runtime', 'data', 'test_data.csv')
 
 
 # pylint: disable=maybe-no-member, too-many-public-methods
@@ -51,7 +52,64 @@ class PresenceAnalyzerViewsTestCase(unittest.TestCase):
         self.assertEqual(resp.content_type, 'application/json')
         data = json.loads(resp.data)
         self.assertEqual(len(data), 2)
-        self.assertDictEqual(data[0], {u'user_id': 10, u'name': u'User 10'})
+        self.assertDictEqual(data[0], {'user_id': 10, 'name': 'User 10'})
+
+    def test_api_mean_time(self):
+        """
+        Test api mean time weekday view for existing user.
+        """
+        resp = self.client.get('/api/v1/mean_time_weekday/10')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        data = json.loads(resp.data)
+        self.assertEqual(
+            data,
+            [
+                ['Mon', 0],
+                ['Tue', 30047.0],
+                ['Wed', 24465.0],
+                ['Thu', 23705.0],
+                ['Fri', 0],
+                ['Sat', 0],
+                ['Sun', 0],
+            ]
+        )
+
+    def test_api_mean_time_404(self):
+        """
+        Test api mean time weekday view for unexisting user.
+        """
+        resp = self.client.get('/api/v1/mean_time_weekday/1')
+        self.assertEqual(resp.status_code, 404)
+
+    def test_api_presence_weekday(self):
+        """
+        Test api presence weekday view for existing user.
+        """
+        resp = self.client.get('/api/v1/presence_weekday/10')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        data = json.loads(resp.data)
+        self.assertEqual(
+            data,
+            [
+                ['Weekday', 'Presence (s)'],
+                ['Mon', 0],
+                ['Tue', 30047.0],
+                ['Wed', 24465.0],
+                ['Thu', 23705.0],
+                ['Fri', 0],
+                ['Sat', 0],
+                ['Sun', 0],
+            ]
+        )
+
+    def test_api_presence_weekday_404(self):
+        """
+        Test api presence weekday view unexisting user.
+        """
+        resp = self.client.get('/api/v1/presence_weekday/1')
+        self.assertEqual(resp.status_code, 404)
 
 
 class PresenceAnalyzerUtilsTestCase(unittest.TestCase):
@@ -85,6 +143,54 @@ class PresenceAnalyzerUtilsTestCase(unittest.TestCase):
             data[10][sample_date]['start'],
             datetime.time(9, 39, 5)
         )
+
+    def test_group_by_weekday(self):
+        """
+        Groups entries by weekday.
+        """
+        sample_data = utils.get_data()
+        result = utils.group_by_weekday(sample_data[10])
+        self.assertListEqual(
+            result,
+            [[], [30047], [24465], [23705], [], [], []]
+        )
+
+    def test_seconds_since_midnight(self):
+        """
+        Test calculating amount of seconds since midnight.
+        """
+        time = datetime.time(1, 2, 3)
+        self.assertEqual(utils.seconds_since_midnight(time), 3723)
+        time = datetime.time(0, 0, 0)
+        self.assertEqual(utils.seconds_since_midnight(time), 0)
+        time = datetime.time(0, 0, 1)
+        self.assertEqual(utils.seconds_since_midnight(time), 1)
+        time = datetime.time(0, 2, 0)
+        self.assertEqual(utils.seconds_since_midnight(time), 120)
+        time = datetime.time(3, 0, 0)
+        self.assertEqual(utils.seconds_since_midnight(time), 10800)
+
+    def test_intervals(self):
+        """
+        Test calculating intervals between two datetime.time obj.
+        """
+        time = datetime.time(0, 0, 3)
+        time2 = datetime.time(1, 2, 3)
+        self.assertEqual(utils.interval(time, time2), 3720)
+        self.assertEqual(utils.interval(time2, time), -3720)
+        self.assertEqual(utils.interval(time2, time2), 0)
+        self.assertGreaterEqual(time2, time)
+
+    def test_mean_time(self):
+        """
+        Test calculating arithmetic mean.
+        """
+        items = [1, 2, 3]
+        self.assertEqual(utils.mean(items), 2.0)
+        items = [-1, -2, -3]
+        self.assertEqual(utils.mean(items), -2.0)
+        items = []
+        self.assertEqual(utils.mean(items), 0)
 
 
 def suite():
